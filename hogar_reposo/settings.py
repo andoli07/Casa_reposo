5cf1e10947264d390cfff59861291ddbeaf84658
kilberblack <kilberblack@gmail.com>
2024-11-26 11:20:50 -0300

    Final commit, Debug = False

--- hogar_reposo/settings.py
+++ hogar_reposo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=o6*wms1dui6&3qy)yv5)%hc%i1eg56s*(99&)gt(dbvx=$e2$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
